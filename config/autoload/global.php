9953305851b92a3e07203a60a2f5642836411fac
George Popoff <popoff96@live.com>
2017-02-19 14:50:29 +0300

    set up database connection

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,11 @@
  */
 
 return [
-    // ...
+    'db' => [
+        'driver'  => 'Pdo',
+        'dsn'     => 'mysql:dbname=momento;host=localhost',
+        'driver_options' => array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"
+        ),
+    ]
 ];
