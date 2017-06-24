8cb7acd42604a0fe85863910a29634a55460e81f
Aleksandr Ploskov <merhalak@gmail.com>
2017-06-24 23:28:17 +0300

    SQL dumps reformat and add '.idea/' to .gitignore

--- data/blogs_dump.sql
+++ data/blogs_dump.sql
@@ -14,5 +14,24 @@ LIMIT 0, 1000
 
 -- Date: 2017-02-19 14:53
 */
-INSERT INTO `blogs` (`id`,`author`,`created`,`text`,`title`,`description`) VALUES (1,1,'18.02.2017','Мой первый блог, который находится в базе данных, представляете, как же это здорово, иметь свой блог, просто супер, вы можете постить что угодно, это просто топ, нонстоп отдых, пляж 2016 лето лучшие треки на ваш мобильный только сегодня универ новые серии как починить сломанный телефон, как собрать лучший в мире онлайн симулятор эрекция продлевается на 100% больше мускулов как у качков типа обамки - обезьянки, который не умеет слушать Максим онлайн без регистрации и смс.','Первый пост!','Мой первый блог, который находится в базе данных, представляете, как же это здорово, иметь свой блог, просто супер, вы можете постить что угодно, это просто топ, нонстоп отдых, пляж 2016 лето лучшие треки на ваш мобильный только сегодня!');
-INSERT INTO `blogs` (`id`,`author`,`created`,`text`,`title`,`description`) VALUES (2,1,'19.02.2017','We are experience troubles with encoding on that page! HELP! What we\'ve should do to make this question marks real characters of cyrilliza?! I\'m tried all ways and they didn\'t help!','Troubles with encoding on this site.','We are experience troubles with encoding on that page! HELP! What we\'ve should do to make this question marks real characters of cyrilliza?! I\'m tried all ways and they didn\'t help!');
+USE `momento`;
+
+INSERT INTO `blogs` (`id`, `author`, `created`, `text`, `title`, `description`)
+VALUES (1, 1, '18.02.2017',
+        'Мой первый блог, который находится в базе данных, представляете, как же это здорово, иметь свой блог, просто
+         супер, вы можете постить что угодно, это просто топ, нонстоп отдых, пляж 2016 лето лучшие треки на ваш
+          мобильный только сегодня универ новые серии как починить сломанный телефон, как собрать лучший в мире онлайн
+           симулятор эрекция продлевается на 100% больше мускулов как у качков типа обамки - обезьянки, который не
+            умеет слушать Максим онлайн без регистрации и смс.',
+        'Первый пост!',
+        'Мой первый блог, который находится в базе данных, представляете, как же это здорово, иметь свой блог, просто
+         супер, вы можете постить что угодно, это просто топ, нонстоп отдых, пляж 2016 лето лучшие треки на ваш
+          мобильный только сегодня!');
+
+INSERT INTO `blogs` (`id`, `author`, `created`, `text`, `title`, `description`)
+VALUES (2, 1, '19.02.2017',
+        'We are experience troubles with encoding on that page! HELP! What we\'ve should do to make this question marks
+         real characters of cyrilliza?! I\'m tried all ways and they didn\'t help!',
+        'Troubles with encoding on this site.',
+        'We are experience troubles with encoding on that page! HELP! What we\'ve should do to make this question marks
+         real characters of cyrilliza?! I\'m tried all ways and they didn\'t help!');
